4a06fb92f80e5bad5a542fc063e13e842de8d8ff
pstanisl <petr.stanislav@gmail.com>
2018-06-12 23:11:35 +0200

    symlink to .vimrc based on the system

--- install.sh
+++ install.sh
@@ -4,6 +4,38 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+function symlink() {
+  src="$1"
+  dest="$2"
+
+  if [ -e "$dest" ]; then
+    if [ -L "$dest" ]; then
+      # Already symlinked -- I'll assume correctly.
+      return
+    else
+      # Rename files with a ".old" extension.
+      echo "$dest already exists, renaming to $dest.old"
+      backup="$dest.old"
+      if [ -e "$backup" ]; then
+        echo "Error: "$backup" already exists. Please delete or rename it."
+        exit 1
+      fi
+      mv -v "$dest" "$backup"
+    fi
+  fi
+  ln -sf "$src" "$dest"
+}
+
+function createSymlinks() {
+    OS=`uname`
+
+    if [[ "${OS}" == "Darwin" ]]; then
+        symlink "$basedir/.vim/vimrc_mac" "$HOME/.vimrc"
+    else
+        symlink "$basedir/.vim/vimrc_main" "$HOME/.vimrc"
+    fi
+}
+
 function doIt() {
     rsync --exclude ".git/" \
         --exclude ".DS_Store" \
@@ -13,6 +45,8 @@ function doIt() {
         --exclude "LICENSE-MIT.txt" \
         -avh --no-perms . ~;
     source ~/.bash_profile;
+
+    createSymlinks;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
