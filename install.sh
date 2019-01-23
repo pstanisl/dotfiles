1a7e27a8015ebbf986300d7744dfdeb3a0d5e8aa
Petr Stanislav <petr.stanislav@gmail.com>
2019-01-23 18:18:46 +0100

    make installing .vimrc

--- install.sh
+++ install.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env bash
 
+basedir="$HOME/.dotfiles"
+bindir="$HOME/bin"
+
+
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-# function symlink() {
-#   src="$1";
-#   dest="$2";
+function symlink() {
+  src="$1";
+  dest="$2";
 
-#   if [ -e "$dest" ]; then
-#     if [ -L "$dest" ]; then
-#       # Already symlinked -- I'll assume correctly.
-#       return;
-#     else
-#       # Rename files with a ".old" extension.
-#       echo "$dest already exists, renaming to $dest.old";
-#       backup="$dest.old";
-#       if [ -e "$backup" ]; then
-#         echo "Error: "$backup" already exists. Please delete or rename it.";
-#         exit 1;
-#       fi;
-#       mv -v "$dest" "$backup";
-#     fi;
-#   fi;
-#   ln -sf "$src" "$dest";
-# }
+  if [ -e "$dest" ]; then
+    if [ -L "$dest" ]; then
+      # Already symlinked -- I'll assume correctly.
+      return;
+    else
+      # Rename files with a ".old" extension.
+      echo "$dest already exists, renaming to $dest.old";
+      backup="$dest.old";
+      if [ -e "$backup" ]; then
+        echo "Error: "$backup" already exists. Please delete or rename it.";
+        exit 1;
+      fi;
+      mv -v "$dest" "$backup";
+    fi;
+  fi;
+  ln -sf "$src" "$dest";
+}
 
-# function createSymlinks() {
-#     OS=`uname`;
+function createSymlinks() {
+    OS=`uname`;
+   
+    echo "Creating symlinks..."
 
-#     if [[ "${OS}" == "Darwin" ]]; then
-#         symlink "./.vim/vimrc_mac" "$HOME/.vimrc";
-#     else
-#         symlink "./.vim/vimrc_main" "$HOME/.vimrc";
-#     fi;
-# }
+    if [[ "${OS}" == "Darwin" ]]; then
+        symlink "$basedir/.vim/.vimrc_mac" "$HOME/.vimrc";
+    else
+        symlink "$basedir/.vim/.vimrc_main" "$HOME/.vimrc";
+    fi;
+}
 
 function doIt() {
     rsync --exclude ".git/" \
@@ -46,8 +52,13 @@ function doIt() {
         -avh --no-perms . ~;
     source ~/.bash_profile;
 
-    # createSymlinks;
+    createSymlinks;
 }
+
+if ! which git >/dev/null ; then
+    echo "Error: git is not installed"
+    exit 1
+fi	
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt;
@@ -59,5 +70,5 @@ else
     fi;
 fi;
 unset doIt;
-# unset createSymlinks;
-# unset symlink;
+unset createSymlinks;
+unset symlink;
