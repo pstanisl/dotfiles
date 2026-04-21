2a61c1fdeb7edc2829ea278bdf38117a7c2fd672
Petr Stanislav <petr.stanislav@gmail.com>
2026-04-21 12:15:58 +0200

    refactor: simplify nvm lazy-loading wrappers

--- dot_zsh/nvm.zsh
+++ dot_zsh/nvm.zsh
@@ -1,9 +1,34 @@
 # NVM lazy load
 #
-if [ -s "$HOME/.nvm/nvm.sh" ]; then
-	#[ -s "$NVM_DIR/bash_completion" ] $$ . "$NVM_DIR/bash_completion"
-    #[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
-	alias nvm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && nvm'
-	alias node='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && node'
-	alias npm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && npm'
+export NVM_DIR="$HOME/.nvm"
+
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  _nvm_lazy_load() {
+    unset -f nvm node npm pi _nvm_lazy_load _nvm_lazy_load_dispatch
+    . "$NVM_DIR/nvm.sh"
+    hash -r
+  }
+
+  _nvm_lazy_load_dispatch() {
+    local cmd="$1"
+    shift
+    _nvm_lazy_load
+    command "$cmd" "$@"
+  }
+
+  nvm() {
+    _nvm_lazy_load_dispatch nvm "$@"
+  }
+
+  node() {
+    _nvm_lazy_load_dispatch node "$@"
+  }
+
+  npm() {
+    _nvm_lazy_load_dispatch npm "$@"
+  }
+
+  pi() {
+    _nvm_lazy_load_dispatch pi "$@"
+  }
 fi
